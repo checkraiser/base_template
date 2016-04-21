f62acd9164df328814c0acb285d0ad17fa3f8a10
Truong Hoang Dung <checkraiser11@gmail.com>
2016-04-21 12:01:59 +0700

    Fix scripts

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 bin/rake bower:install['--allow-root']
-NODE_ENV=development forever start ./realtime/realtime-server.js
+#NODE_ENV=development forever start ./realtime/realtime-server.js
 bin/rails s
 
 
---
+++ start_realtime.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+NODE_ENV=development forever start ./realtime/realtime-server.js
+
+
