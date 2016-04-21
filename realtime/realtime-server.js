48fe8a3b1ca7b9cdc6c782826b4b6c2bcde9bb7c
Truong Hoang Dung <checkraiser11@gmail.com>
2016-04-21 11:56:54 +0700

    Fix proxy

--- realtime/realtime-server.js
+++ realtime/realtime-server.js
@@ -1,10 +1,8 @@
 var redis = require('redis').createClient('6379', 'redis')
 redis.subscribe('message-created');
 redis.subscribe('contacts-loaded');
-var io = require('socket.io').listen(process.env.PORT || 5001);
-io.configure(function() {
-    io.set('origins', '*:*');
-});
+var io = require('socket.io').listen(process.env.PORT || 5001, {origins: '*:*'});
+
 io.on('connection', function(socket){
     console.log('connected socket')
     socket.on('disconnect', function(){
