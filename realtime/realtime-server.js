0ef1597e827102c14bd27ffc9bb674ec858d03d2
Truong Hoang Dung <checkraiser11@gmail.com>
2016-04-21 07:01:44 +0700

    Fix socket.io

--- realtime/realtime-server.js
+++ realtime/realtime-server.js
@@ -2,6 +2,9 @@ var redis = require('redis').createClient('6379', 'redis')
 redis.subscribe('message-created');
 redis.subscribe('contacts-loaded');
 var io = require('socket.io').listen(process.env.PORT || 5001);
+io.configure(function() {
+    io.set('origins', '*:*');
+});
 io.on('connection', function(socket){
     console.log('connected socket')
     socket.on('disconnect', function(){
