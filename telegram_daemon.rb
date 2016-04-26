37d59f50a1c74f586e3d60812f85aa07ac11037b
Truong Hoang Dung <checkraiser11@gmail.com>
2016-04-26 13:49:20 +0700

    Add em-redis

--- telegram_daemon.rb
+++ telegram_daemon.rb
@@ -1,6 +1,6 @@
 require 'eventmachine'
 require 'telegram'
-require 'redis'
+require 'em-hiredis'
 require 'json'
 require 'yaml'
 #SETTINGS = YAML::load_file File.join(__dir__, 'config', 'server_settings.yml')
@@ -8,7 +8,7 @@ require 'yaml'
 app_home = ENV['APP_HOME']
 #redis_host = SETTINGS[env]['redis_host']
 
-$redis = Redis.new(:host => 'redis', :port=> 6379)
+#$redis = Redis.new(:host => 'redis', :port=> 6379, :timeout => 0)
 
 def push_event(event_name, user_ids, event_type, data = nil, guest_ids = [])
   event = {}
@@ -22,7 +22,37 @@ end
 def json_convert(contact)
   {name: contact.name, username: contact.name, phone: contact.phone}
 end
+
+def process_telegram(telegram)
+  telegram.contacts.each do |contact|
+    push_event('contacts-loaded', [], contact.class, json_convert(contact))
+    puts json_convert(contact)
+  end
+
+  telegram.chats.each do |chat|
+    puts chat
+  end
+
+  # Event listeners
+  # When you've received a message:
+  telegram.on[Telegram::EventType::RECEIVE_MESSAGE] = Proc.new { |event|
+    # `tgmessage` is TelegramMessage instance
+    message = event.message
+    puts json_convert(message.from)
+    puts json_convert(message.to)
+    push_event('message-created', [], event.tgmessage.class, {text: message.text, from: json_convert(message.from), to: json_convert(message.to)} )
+    # redis.push ()
+  }
+  # When you've sent a message:
+  telegram.on[Telegram::EventType::SEND_MESSAGE]= Proc.new { |event|
+    message = event.message
+    puts json_convert(message.from)
+    puts json_convert(message.to)
+    push_event('message-created', [], event.tgmessage.class, {text: message.text, from: json_convert(message.from), to: json_convert(message.to)})
+  }
+end
 EM.run do
+  $redis = EM::Hiredis.connect "redis://redis:6379"
   telegram = Telegram::Client.new do |cfg|
     cfg.daemon = "#{app_home}/tg/bin/telegram-cli"
     cfg.key = "#{app_home}/tg/tg_server.pub"
@@ -33,32 +63,12 @@ EM.run do
     # This block will be executed when initialized.
 
     # See your telegram profile
-
-    telegram.contacts.each do |contact|
-      push_event('contacts-loaded', [], contact.class, json_convert(contact))
-      puts json_convert(contact)
-    end
-
-    telegram.chats.each do |chat|
-      puts chat
-    end
-
-    # Event listeners
-    # When you've received a message:
-    telegram.on[Telegram::EventType::RECEIVE_MESSAGE] = Proc.new { |event|
-      # `tgmessage` is TelegramMessage instance
-      message = event.message
-      puts json_convert(message.from)
-      puts json_convert(message.to)
-      push_event('message-created', [], event.tgmessage.class, {text: message.text, from: json_convert(message.from), to: json_convert(message.to)} )
-      # redis.push ()
-    }
-    # When you've sent a message:
-    telegram.on[Telegram::EventType::SEND_MESSAGE]= Proc.new { |event|
-      message = event.message
-      puts json_convert(message.from)
-      puts json_convert(message.to)
-      push_event('message-created', [], event.tgmessage.class, {text: message.text, from: json_convert(message.from), to: json_convert(message.to)})
+    process_telegram telegram
+    $redis.pubsub.subscribe("send-message") { |msg|
+      data = JSON.parse(msg)
+      telegram.msg(data['user'], data['msg']) do |success, dat|
+        puts dat
+      end
     }
   end
 end
